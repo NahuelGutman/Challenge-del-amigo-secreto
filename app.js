6905cb69576f327a67bed99f7f5f88d3bbfabd12
Nahuel M. Gutman <nahuelmgutman@gmail.com>
2025-09-09 10:07:35 -0300

    Versiones 1.0

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const input = document.getElementById('amigo');
+const amigos = [];
+const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById("resultado");
+
+
+function agregarAmigo(){
+    if(input.value === ""){
+        alert("Ingresa un nombre para poder continuar")
+    } else{
+    amigos.push(input.value);
+    listaAmigos.innerHTML += `<li>${input.value}</li>`;
+    }
+}
+
+function sortearAmigo(){
+    //const random = 
+    const random = Math.floor((Math.random() * amigos.length));
+    const amigoSecreto = amigos[random];
+    resultado.innerHTML = `El amigo secreto es ${amigoSecreto}`
+}
+
+
